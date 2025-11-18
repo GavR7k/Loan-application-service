d2dea0316789f15f3a039997d613af82e9fe8db1
gavr7k <gavrilovich2002@gmail.com>
2025-11-18 13:18:17 +0300

    Создана сущность LoanEntity

---
+++ backend/LoanService/LoanService/Models/LoanEntity.cs
@@ -0,0 +1,36 @@
+﻿namespace LoanService.Models
+{
+    public class LoanEntity
+    {
+        //Уникальный индентификатор
+        public int Id { get; set; }
+
+        //Статус заявки
+        public LoanStatus Status { get; set; }
+
+        //Номер заявки
+        public string Number { get; set; }
+
+        //Сумма займа
+        public decimal Amount { get; set; }
+
+        //Срок займа (в днях)
+        public int TermValue { get; set; }
+
+        //Процентная ставка
+        public decimal InterestValue { get; set; }
+
+        //Дата и время создания 
+        public DateTimeOffset CreatedAt { get; set; }
+
+        //Дата и время обновления
+        public DateTimeOffset ModifiedAt { get; set; }
+    }
+
+    public enum LoanStatus
+    {
+        Published = 1,
+        Unpublished = 0
+    }
+
+}
