c13f4ffdf7bd423511481df0fafe84870b4f8862
gavr7k <gavrilovich2002@gmail.com>
2025-11-18 13:46:41 +0300

    создание docker-compose.yml c настройкой postgresql , добавление строки подключения к бд в appsettings.json, cсоздание контекста данных и регистрация его в program.cs

---
+++ backend/LoanService/LoanService/Data/LoanDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LoanService.Models;
+
+namespace LoanService.Data
+{
+    public class LoanDbContext : DbContext
+    {
+        public LoanDbContext(DbContextOptions<LoanDbContext> options) : base(options)
+        {
+        }
+        public DbSet<LoanEntity> LoanEntities => Set<LoanEntity>();
+
+    }
+}
