aa2405758b62def481da5d7e94ce789f47844448
gavr7k <gavrilovich2002@gmail.com>
2025-11-19 20:00:06 +0300

    Создание разметки для фильтров, создние метода fetchLoans() для обработки фильтров разных типов + запросы на веб апи

--- frontend/src/views/LoanList.vue
+++ frontend/src/views/LoanList.vue
@@ -1,6 +1,44 @@
 <template>
+  <!-- Фильтры -->
+  <div class="filters">
+    <h2>Фильтры</h2>
+    <div class="filters-item">
+      <label>
+        Статус:
+        <select v-model="filterStatus" @change="fetchLoans">
+          <option value="0">Все</option>
+          <option value="1">Опубликованные</option>
+          <option value="2">Снятые с публикации</option>
+        </select>
+      </label>
+    </div>
+    <div class="filters-item">
+      <label>
+        Сумма от:
+        <input type="number" v-model.number="filterMinAmount" @input="fetchLoans" />
+      </label>
+
+      <label>
+        до:
+        <input type="number" v-model.number="filterMaxAmount" @input="fetchLoans" />
+      </label>
+    </div>
+    <div class="filters-item">
+      <label>
+        Срок займа от:
+        <input type="number" v-model.number="filterMinTerm" @input="fetchLoans" />
+      </label>
+      <label>
+        до:
+        <input type="number" v-model.number="filterMaxTerm" @input="fetchLoans" />
+      </label>
+
+    </div>
+  </div>
+
+  <!-- Таблица с заявками -->
   <div class="loan-list">
-    <h1>Список заявок</h1>
+    <h2>Список заявок</h2>
     <table>
       <thead>
         <tr>
@@ -32,7 +70,6 @@
   </div>
 </template>
 
-
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 
@@ -48,14 +85,15 @@ interface Loan {
 
 const loans = ref<Loan[]>([]);
 
-onMounted(async () => {
-  const res = await fetch('http://localhost:5007/api/loans');
-  loans.value = await res.json();
-});
-
 function formatDate(dateStr: string): string {
   const date = new Date(dateStr);
-  return date.toLocaleDateString('ru-RU');
+  return date.toLocaleDateString('ru-RU', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
 }
 
 async function toggleStatus(loan: Loan) {
@@ -71,6 +109,37 @@ async function toggleStatus(loan: Loan) {
   const updatedLoan = await response.json();
   loan.status = updatedLoan.status;
 }
+
+// Реактивные переменные для фильтрации
+
+const filterStatus = ref<string>('0');
+const filterMinAmount = ref<number | null>(null);
+const filterMaxAmount = ref<number | null>(null);
+const filterMinTerm = ref<number | null>(null);
+const filterMaxTerm = ref<number | null>(null);
+
+async function fetchLoans() {
+
+  const params = new URLSearchParams();
+  // статус
+  if (filterStatus.value === '1') params.append('status', '1'); // опубликованные
+  else if (filterStatus.value === '2') params.append('status', '0'); // снятые
+
+  // сумма
+  if (filterMinAmount.value !== null) params.append('minAmount', filterMinAmount.value.toString());
+  if (filterMaxAmount.value !== null) params.append('maxAmount', filterMaxAmount.value.toString());
+
+  // срок
+  if (filterMinTerm.value !== null) params.append('minTerm', filterMinTerm.value.toString());
+  if (filterMaxTerm.value !==null) params.append('maxTerm',filterMaxTerm.value.toString());
+
+  // запрос на веб-апи
+  const url = `http://localhost:5007/api/loans?${params.toString()}`;
+  const res = await fetch(url);
+  loans.value = await res.json();
+}
+
+onMounted(fetchLoans);
 
 </script>
 
